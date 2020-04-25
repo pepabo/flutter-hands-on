7f8989ef36922b564e4c1cd6d425012998e09106
tokky <otokita@pepabo.com>
2020-04-25 15:51:00 +0900

    商品の説明があったら表示する

--- lib/pages/product_detail.dart
+++ lib/pages/product_detail.dart
@@ -17,21 +17,44 @@ class ProductDetail extends StatelessWidget {
 
   Widget _body(BuildContext context, Product product) {
     return SingleChildScrollView(
-      child: Center(
-        child: Column(
-          children: <Widget>[
-            Image.network(product.sampleImageUrl),
-            Text(
-              product.title,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-              ),
+      child: Column(
+        children: <Widget>[
+          Center(
+            child: Image.network(product.sampleImageUrl),
+          ),
+          Text(
+            product.title,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
             ),
-            Text(product.item.humanizeName),
-            Text("${product.price.toString()}円"),
-            Text("作った人: ${product.material.user.name}"),
-          ],
-        ),
+          ),
+          Text(product.item.humanizeName),
+          Text("${product.price.toString()}円"),
+          Text("作った人: ${product.material.user.name}"),
+          product.material.description.isEmpty
+              ? Container()
+              : _descriptionSection(context, product),
+        ],
+      ),
+    );
+  }
+
+  Widget _descriptionSection(BuildContext context, Product product) {
+    return Container(
+      margin: EdgeInsets.only(top: 16.0),
+      child: Column(
+        children: <Widget>[
+          Text(
+            "このアイテムについて",
+            style: TextStyle(
+              color: Colors.grey,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(16.0),
+            child: Text(product.material.description),
+          ),
+        ],
       ),
     );
   }
